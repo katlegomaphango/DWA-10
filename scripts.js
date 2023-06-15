e810d2ad44ab9cff8dc585306abce5c7d714e2a1
katlego Maphango <mphangokatlego08@gmail.com>
2023-06-16 00:05:54 +0200

    simple event handlers

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log('it works')
+
+const count = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+add.addEventListener('click', () => {
+    count.value++
+})
+
+subtract.addEventListener('click', () => {
+    count.value--
+})
