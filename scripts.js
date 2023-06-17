573d7ba87f93de0740f5d6fffdbb62d2c2a060d9
Katlego Maphango <mphangokatlego08@gmail.com>
2023-06-17 16:15:34 +0200

    show alert

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const count = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 const reset = document.querySelector('[data-key="reset"]')
+const alertMsg = document.querySelector('sl-alert')
 
 add.addEventListener('click', () => {
     count.value++
@@ -15,4 +16,7 @@ subtract.addEventListener('click', () => {
 
 reset.addEventListener('click', () => {
     count.value = 0
+
+    alertMsg.open = true
+    setTimeout(() => { alertMsg.open = false }, 3000)
 })
